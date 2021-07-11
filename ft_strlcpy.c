325d816f09a59b4f85263722560392418699503d
Hayato Kawakita <hkawakit@c1r5s6.42tokyo.jp>
2021-07-12 01:20:03 +0900

    updated

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,9 +1,11 @@
-#include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	len;
 
+	if (src == NULL)
+		return (0);
 	len = 1;
 	while (*src && len < dstsize)
 	{
