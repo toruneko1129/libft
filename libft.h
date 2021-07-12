98b830e3707f63c25e725aa759e62594ba3d16ca
Hayato Kawakita <hkawakit@c1r5s6.42tokyo.jp>
2021-07-12 21:48:17 +0900

    updated

--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
