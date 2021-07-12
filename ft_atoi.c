517cc9fa1898ea8510fc7d659a97f7cd1e9bf878
Hayato Kawakita <hkawakit@c1r5s6.42tokyo.jp>
2021-07-12 21:34:48 +0900

    updated

---
+++ ft_atoi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	long	res;
+	int		sign;
+
+	res = 0;
+	sign = 1;
+	while ((9 <= *str && *str <= 13) || *str == 32)
+		++str;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		++str;
+	}
+	while (ft_isdigit(*str))
+		res = res * 10 + *(str++) - '0';
+	res *= sign;
+	return (res);
+}
