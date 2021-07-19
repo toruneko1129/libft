253efe1fc8bb238af94333b7a789e17cb05fcb08
toruneko1129 <kawakita.hayato@gmail.com>
2021-07-19 15:44:09 +0900

    updated

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 15:39:46 by hkawakit          #+#    #+#             */
+/*   Updated: 2021/07/19 15:41:35 by hkawakit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*dstptr;
-	unsigned char	*srcptr;
-	size_t			size;
+	unsigned char		*dstptr;
+	const unsigned char	*srcptr;
+	size_t				size;
 
 	if (!len || dst == src)
 		return (dst);
@@ -23,8 +35,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		++srcptr;
 	}
 	while (len--)
-	{
 		*(--dstptr) = *(--srcptr);
-	}
 	return (dst);
 }
