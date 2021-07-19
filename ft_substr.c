56ed6ba3b8612c3778c1a12ddbbc3e29fda279b3
toruneko1129 <kawakita.hayato@gmail.com>
2021-07-19 17:56:41 +0900

    updated

--- ft_substr.c
+++ ft_substr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 17:22:00 by hkawakit          #+#    #+#             */
+/*   Updated: 2021/07/19 17:25:48 by hkawakit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -5,8 +17,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr;
 
 	if (ft_strlen(s) < start)
-		start = ft_strlen(s);
-	if (ft_strlen(s) < start + len)
+		start = (unsigned int)ft_strlen(s);
+	if (ft_strlen(s) < len + (size_t)start)
 		len = ft_strlen(s) - start;
 	ptr = (char *)ft_calloc(len + 1, sizeof(char));
 	if (ptr == NULL)
