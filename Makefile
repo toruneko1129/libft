1ae2e1d058e771272734681ba7dd85dded21a2b6
toruneko1129 <kawakita.hayato@gmail.com>
2021-07-19 19:58:15 +0900

    updated

--- Makefile
+++ Makefile
@@ -39,7 +39,11 @@ BNSRCS	+=	ft_lstnew_bonus.c \
 			ft_lstadd_front_bonus.c \
 			ft_lstsize_bonus.c \
 			ft_lstlast_bonus.c \
-			ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c
+			ft_lstadd_back_bonus.c \
+			ft_lstdelone_bonus.c \
+			ft_lstclear_bonus.c \
+			ft_lstiter_bonus.c \
+			ft_lstmap_bonus.c
 BNOBJS	=	$(BNSRCS:.c=.o)
 CC		=	gcc
 CFLAG	=	-Wall -Wextra -Werror
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:44:35 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/19 15:44:49 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/19 19:50:03 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
