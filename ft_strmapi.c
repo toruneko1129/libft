e1dc33fa73aa5623944a28d72b7888d7b56a1f48
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-21 15:17:46 +0900

    updated

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 18:27:26 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/19 18:29:17 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/21 15:12:48 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t			len;
 	unsigned int	i;
 
+	if (s == NULL)
+		return (NULL);
 	len = ft_strlen(s);
+	if (len && f == NULL)
+		return (NULL);
 	res = (char *)ft_calloc(len + 1, sizeof(char));
 	if (res == NULL)
 		return (NULL);
-	i = -1;
-	while (++i < len)
+	i = 0;
+	while (i < len)
+	{
 		res[i] = (*f)(i, s[i]);
+		++i;
+	}
 	return (res);
 }
