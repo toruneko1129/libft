29abe8d7ac8134e1defa8459a3c86c0416958d85
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-22 00:02:15 +0900

    updated

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,22 +6,23 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 19:23:12 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/21 16:20:57 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/22 00:01:05 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//SIGSEGV
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	**begin;
 	t_list	*last;
 
 	if (lst == NULL || new == NULL)
 		return ;
-	begin = lst;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
 	last = ft_lstlast(*lst);
-	last = new;
-	lst = begin;
+	last->next = new;
 }
