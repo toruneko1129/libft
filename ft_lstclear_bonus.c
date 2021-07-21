ec248fa70977f3edb0dd3201cbc00c08d093a74a
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-21 13:47:53 +0900

    updated

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 19:47:08 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/19 22:02:47 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/21 13:47:44 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*ptr;
-	t_list	*nxt;
 
 	ptr = *lst;
 	while (ptr != NULL)
 	{
-		nxt = ptr->next;
+		*lst = ptr->next;
 		ft_lstdelone(ptr, del);
-		ptr = nxt;
+		ptr = *lst;
 	}
 	*lst = NULL;
 }
