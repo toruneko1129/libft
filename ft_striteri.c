8c07ba66e86dff4f68abd7ea2767294d93bb4e1b
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-21 15:26:09 +0900

    updated

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 18:35:44 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/19 18:36:21 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/21 15:25:16 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	size_t			len;
 	unsigned int	i;
 
+	if (s == NULL || f == NULL)
+		return ;
 	len = ft_strlen(s);
-	i = -1;
-	while (++i < len)
+	i = 0;
+	while (i < len)
+	{
 		(*f)(i, s + i);
+		++i;
+	}
 }
