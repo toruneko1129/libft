3fe05833081a2924c53526ec5a9b8cb262aee7d5
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-21 12:54:34 +0900

    updated

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:39:46 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/19 15:41:35 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/21 12:54:16 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,19 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*dstptr;
 	const unsigned char	*srcptr;
-	size_t				size;
 
 	if (!len || dst == src)
 		return (dst);
 	dstptr = (unsigned char *)dst;
 	srcptr = (unsigned char *)src;
-	if (dstptr <= srcptr)
+	if (dstptr < srcptr)
 	{
 		while (len--)
 			*dstptr++ = *srcptr++;
 		return (dst);
 	}
-	size = len;
-	while (size--)
-	{
-		++dstptr;
-		++srcptr;
-	}
+	dstptr += len;
+	srcptr += len;
 	while (len--)
 		*(--dstptr) = *(--srcptr);
 	return (dst);
