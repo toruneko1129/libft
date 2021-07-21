1ce90652d908aaebd231fee4382c08d275339109
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-21 15:50:19 +0900

    updated

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,15 +6,25 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 18:41:20 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/21 15:32:00 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/21 15:48:47 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#define MAX_SIZE 1000000
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	size_t	len;
+
 	if (s == NULL)
 		return ;
-	write(fd, s, ft_strlen(s));
+	len = ft_strlen(s);
+	while (len > MAX_SIZE)
+	{
+		write(fd, s, MAX_SIZE);
+		s += MAX_SIZE;
+		len -= MAX_SIZE;
+	}
+	write(fd, s, len);
 }
