8c868a338dc3d9f09391ec4ede7f4559965ca6d9
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-22 20:29:12 +0900

    updated

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 17:22:00 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/21 14:14:19 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/22 20:26:49 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	size = ft_strlen(s);
 	if (size < start)
-		start = (unsigned int)size;
+		start = size;
 	if (size < len + start)
 		len = size - start;
 	ptr = (char *)ft_calloc(len + 1, sizeof(char));
