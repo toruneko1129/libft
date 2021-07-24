d18a3945fc075b34263f8a209efc5c19fc560692
Hayato Kawakita <hkawakit@c1r29s6.42tokyo.jp>
2021-07-24 12:45:59 +0900

    updated

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 19:56:29 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/07/22 02:21:48 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/07/24 12:42:06 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 	t_list	*nxt;
+	t_list	**last;
 
 	if (f == NULL)
 		return (NULL);
 	res = NULL;
+	last = &res;
 	while (lst != NULL)
 	{
 		nxt = ft_lstnew((*f)(lst->content));
@@ -28,7 +30,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			ft_lstclear(&res, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&res, nxt);
+		ft_lstadd_back(last, nxt);
+		last = &((*last)->next);
 		lst = lst->next;
 	}
 	return (res);
